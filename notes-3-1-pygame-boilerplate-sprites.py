849c3a4a0d2fe8e22df9f7fea8655799893ac339
Aisha Du <aishadu2@gmail.com>
2024-04-29 12:41:54 -0700

    Made snowflakes fall

--- notes-3-1-pygame-boilerplate-sprites.py
+++ notes-3-1-pygame-boilerplate-sprites.py
@@ -2,11 +2,18 @@
 #    - boilerplate
 #    - Sprite class
 
+import random
 import pygame
 
 WIDTH = 1280  # Pixels
 HEIGHT = 760
 SCREEN_SIZE = (WIDTH, HEIGHT)
+
+LOGO_IMAGES = [
+    pygame.image.load("./Images/dvd-logo.png"),
+    pygame.image.load("./Images/dvd-logo-2.svg"),
+    pygame.image.load("./Images/dvd-logo-3.gif"),
+]
 
 
 class Dvdlogo(pygame.sprite.Sprite):
@@ -15,19 +22,18 @@ class Dvdlogo(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.image = pygame.image.load("./Images/dvd-logo.png")
-
+        self.image = random.choice(LOGO_IMAGES)
         # sets the x and y to 0
         #    first position of the image is in the top right
         self.rect = self.image.get_rect()
 
-        self.rect.centerx = WIDTH
-        self.rect.centery = HEIGHT
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(0, HEIGHT - self.rect.height)
 
         # How much position changes over time
         #    - pixels per tick
-        self.vel_x = 3
-        self.vel_y = 3
+        self.vel_x = random.choice([-6, -5, -4, -3, 3, 4, 5, 6])
+        self.vel_y = random.choice([-6, -5, -4, -3, 3, 4, 5, 6])
 
     def update(self):
         # Update position of Dvdlogo
@@ -38,19 +44,50 @@ class Dvdlogo(pygame.sprite.Sprite):
         # Right side of the screen
         #     - if the right edge of dvdlogo > WIDTH
         #          - switch the direction (+vel-x -> -vel-x)
-        if self.rect.right >= WIDTH:
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
             self.vel_x = -self.vel_x
-        if self.rect.left <= 0:
+        if self.rect.left < 0:
+            self.rect.left = 0
             self.vel_x = -self.vel_x
-        if self.rect.bottom >= HEIGHT:
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
             self.vel_y = -self.vel_y
-        if self.rect.top <= 0:
+        if self.rect.top < 0:
+            self.rect.top = 0
             self.vel_y = -self.vel_y
 
-        # Top side
-        # Bottom side
+    def incr_velocity(self):
+        """Increases the velocity in the proper direction"""
+        if self.vel_x < 0:
+            self.vel_x -= 1
+        elif self.vel_x > 0:
+            self.vel_x += 1
+        else:
+            self.vel_x = random.choice([-1, 1])
 
-        print(self.rect.x, self.rect.y)
+        if self.vel_y < 0:
+            self.vel_y -= 1
+        elif self.vel_y > 0:
+            self.vel_y += 1
+        else:
+            self.vel_y = random.choice([-1, 1])
+
+    def decr_velocity(self):
+        """Decreases the velocity in the proper direction"""
+        if self.vel_x < 0:
+            self.vel_x += 1
+        elif self.vel_x > 0:
+            self.vel_x -= 1
+        else:
+            self.vel_x = random.choice([-1, 1])
+
+        if self.vel_y < 0:
+            self.vel_y += 1
+        elif self.vel_y > 0:
+            self.vel_y -= 1
+        else:
+            self.vel_y = random.choice([-1, 1])
 
 
 def start():
@@ -73,10 +110,9 @@ def start():
     done = False
     clock = pygame.time.Clock()
 
-    dvdlogo = Dvdlogo()
-
     all_sprites = pygame.sprite.Group()
-    all_sprites.add(dvdlogo)
+
+    all_sprites.add(Dvdlogo())
 
     pygame.display.set_caption("DVD Screen Saver")
 
@@ -86,6 +122,20 @@ def start():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+            # Listen for the keyboard space bar to be pressed
+            # spawn a new dvdlogo object
+            if event.type == pygame.KEYDOWN:
+                if pygame.key.get_pressed()[pygame.K_SPACE]:
+                    all_sprites.add(Dvdlogo())
+                if pygame.key.get_pressed()[pygame.K_1]:
+                    for sprite in all_sprites:
+                        sprite.incr_velocity()
+                if pygame.key.get_pressed()[pygame.K_2]:
+                    for sprite in all_sprites:
+                        sprite.decr_velocity()
+                if pygame.key.get_pressed()[pygame.K_BACKSPACE]:
+                    if len(all_sprites) > 0:
+                        all_sprites.spritedict.popitem()
 
         # --- Update the world state
         all_sprites.update()
---
+++ pygame-exercuse-snow.py
@@ -0,0 +1,73 @@
+import pygame as pg
+import random
+# --CONSTANTS--
+# COLOURS
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+EMERALD = (21, 219, 147)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
+WIDTH = 1280  # Pixels
+HEIGHT = 720
+SCREEN_SIZE = (WIDTH, HEIGHT)
+class Snowflake(pg.sprite.Sprite):
+    def __init__(self, size: int):
+        """
+        Params:
+            size: diameter of the snowflake in pixels
+        """
+        #super class constructor
+        super().__init__()
+        # create a blank surface
+        self.image = pg.Surface((size, size)) # width and height
+        # and draw a circle inside of it
+        pg.draw.circle(self.image, WHITE, (size // 2, size //2),  size // 2)
+        self.rect = self.image.get_rect()
+        #spawn it in the middle of the screen
+        # self.rect.centerx = WIDTH // 2
+        # self.rect.centery = HEIGHT // 2
+        self.rect.x = random.randrange(0, WIDTH)
+        self.rect.y = random.randrange(0, HEIGHT)
+        self.vel_y = -2
+    def update(self):
+        self.rect.y -= self.vel_y
+        # If it falls off the bottom, "TELEPORT IT" up to the top
+        if self.rect.bottom >= HEIGHT + 10:
+            self.rect.y = -10
+            self.rect.x = self.rect.x
+def start():
+    """Environment Setup and Game Loop"""
+    pg.init()
+    # --Game State Variables--
+    screen = pg.display.set_mode(SCREEN_SIZE)
+    done = False
+    clock = pg.time.Clock()
+    # All sprites go in this sprite Group
+    all_sprites = pg.sprite.Group()
+    # add one snowflake to the all_sprites group
+    for _ in range(100):
+        all_sprites.add(Snowflake(10))
+    pg.display.set_caption("Snowfall Landscape")
+    # --Main Loop--
+    while not done:
+        # --- Event Listener
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+        # --- Update the world state
+        # update the state of all sprites
+        all_sprites.update()
+        # --- Draw items
+        screen.fill(BLACK)
+        # draw all the sprites
+        all_sprites.draw(screen)
+        # Update the screen with anything new
+        pg.display.flip()
+        # --- Tick the Clock
+        clock.tick(60)  # 60 fps
+def main():
+    start()
+if __name__ == "__main__":
+    main()
